929ea5f84fba8d213b842b8d8601447c486c9299
Suji <Suji@LAPTOP-EBTTH9OR>
2025-12-17 19:03:18 +0530

    commit 1

---
+++ codejava/src/tnsif/introofjava/basiccode.java
@@ -0,0 +1,24 @@
+package tnsif.introofjava;
+
+public class basiccode {
+	public static void main(String[] args) {
+		int age=21;
+		double salary=50000.00;
+		boolean ispassed=true;
+		char grade='A';
+		double nweSalary=age;//data widening
+		int roundSalary=(int)salary;//data narrowing
+		//Arithmetic operation
+		int increment=5000;
+		int total=roundSalary+increment;
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+				
+	}
+		
+	}
+
+
+
